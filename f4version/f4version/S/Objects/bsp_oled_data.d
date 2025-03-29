d4c4aae88170236fd8bd8219a1aa0b6dc62adc34
cangqiong <2795922402@qq.com>
2025-03-29 22:27:26 +0800

    优化移动函数，将它与游戏结束函数放在一起，使得判断不易出错，优化控制方向函数，使得延迟大大减少

---
+++ f4version/f4version/S/Objects/bsp_oled_data.d
@@ -0,0 +1,3 @@
+.\objects\bsp_oled_data.o: Hardware\bsp_OLED_Data.c
+.\objects\bsp_oled_data.o: Hardware\bsp_OLED_Data.h
+.\objects\bsp_oled_data.o: E:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ f4version/f4version/S/System/bsp_Timer.h
@@ -0,0 +1,7 @@
+#ifndef __BSP_TIMER_H
+#define __BSP_TIMER_H
+
+void Timer_Init(void);
+
+
+#endif
